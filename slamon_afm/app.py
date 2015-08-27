770e915d79dff354156331fac7ef867835f10363
Jouni Kuusisto <jouni.kuusisto@f-secure.com>
2015-08-27 13:19:12 +0300

    Configurable logging: level, format and the target (file/stderr).

--- slamon_afm/app.py
+++ slamon_afm/app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 
 from slamon_afm.models import db
@@ -12,6 +13,9 @@ class DefaultConfig(object):
     AGENT_RETURN_TIME = 60
     AGENT_ACTIVE_THRESHOLD = 300
     AUTO_CREATE = True
+    LOG_FILE = None
+    LOG_LEVEL = logging.DEBUG
+    LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message).120s'
 
 
 def create_app(config=None, config_file=None):
@@ -19,7 +23,7 @@ def create_app(config=None, config_file=None):
     Create AFM Flask application instance and init the database
     """
 
-    # setup app & configuration
+    # setup app & load configuration
     app = Flask(__name__)
     app.config.from_object(DefaultConfig)
     app.config.from_envvar('SLAMON_AFM_CFG', silent=True)
@@ -27,6 +31,14 @@ def create_app(config=None, config_file=None):
         app.config.from_pyfile(config_file)
     if config:
         app.config.update(**config)
+
+    # setup logging according to configuration
+    app.logger_name = 'slamon_afm'
+    handler = logging.FileHandler(app.config['LOG_FILE']) if app.config['LOG_FILE'] else logging.StreamHandler()
+    handler.setLevel(app.config['LOG_LEVEL'])
+    handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+    app.logger.addHandler(handler)
 
     # register app routes
     app.register_blueprint(agent_routes.blueprint)
