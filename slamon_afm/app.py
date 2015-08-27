a65a58006fc0dac960e822436649215f94e2d96a
Jouni Kuusisto <jouni.kuusisto@f-secure.com>
2015-08-27 11:23:49 +0300

    Possibility to define config file location in env or cli

--- slamon_afm/app.py
+++ slamon_afm/app.py
@@ -8,12 +8,13 @@ class DefaultConfig(object):
     """
     Container for default configuration values
     """
-    AGENT_RETURN_TIME = 5
-    AGENT_ACTIVE_THRESHOLD = 500
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    AGENT_RETURN_TIME = 60
+    AGENT_ACTIVE_THRESHOLD = 300
+    AUTO_CREATE = True
 
 
-def create_app(config=None):
+def create_app(config=None, config_file=None):
     """
     Create AFM Flask application instance and init the database
     """
@@ -21,8 +22,10 @@ def create_app(config=None):
     # setup app & configuration
     app = Flask(__name__)
     app.config.from_object(DefaultConfig)
+    app.config.from_envvar('SLAMON_AFM_CFG', silent=True)
+    if config_file:
+        app.config.from_pyfile(config_file)
     if config:
-        print("Loading custom Config")
         app.config.update(**config)
 
     # register app routes
@@ -30,6 +33,12 @@ def create_app(config=None):
     app.register_blueprint(bpms_routes.blueprint)
     app.register_blueprint(status_routes.blueprint)
     app.register_blueprint(dashboard_routes.blueprint)
+
+    # set to auto create tables before first request
+    if app.config['AUTO_CREATE']:
+        @app.before_first_request
+        def create_database():
+            db.create_all()
 
     # register app for Flask-SQLAlchemy DB
     db.init_app(app)
--- slamon_afm/cli.py
+++ slamon_afm/cli.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 import argparse
-import logging
+
+import os.path
 
 from slamon_afm.app import create_app
 from slamon_afm.models import db
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-
     parser = argparse.ArgumentParser(description='SLAMon Agent Fleet Manager')
-    parser.add_argument('--database-uri', type=str, default=None)
+    parser.add_argument('--database-uri', type=str, default=None,
+                        help='Set the AFM database URI, defaults to in memory sqlite')
+    parser.add_argument('--config', '-c', type=str, default=None,
+                        help='Load AFM configuration from a file')
 
     subparsers = parser.add_subparsers()
 
     run_parser = subparsers.add_parser('run', help='Run AFM',
                                        description='Run an instance of an Agent Fleet Manager '
                                                    'that listens to given host address')
-    run_parser.add_argument('host', action='store', help='Host name or address e.g. localhost or 127.0.0.1')
-    run_parser.add_argument('port', type=int, default=8080, help='Listening port')
+    run_parser.add_argument('host', type=str, help='Host name or address e.g. localhost or 127.0.0.1')
+    run_parser.add_argument('port', type=int, default=8080, nargs='?', help='Listening port, defaults to 8080')
     run_parser.set_defaults(func=run_afm)
 
     create_parser = subparsers.add_parser('create-tables', help='Create SQL tables',
@@ -32,27 +34,21 @@ def main():
     args = parser.parse_args()
 
     if not hasattr(args, 'func'):
+        # No function defined, print usage and exit
         parser.print_help()
         exit(1)
 
-    app = create_app()
+    app = create_app(config_file=os.path.abspath(args.config) if args.config else None)
 
     if args.database_uri:
+        # override the database URI
         app.config.update(SQLALCHEMY_DATABASE_URI=args.database_uri)
 
     args.func(app, args)
 
 
 def run_afm(app, args):
-    # with app.app_context():
-    #    import slamon_afm.tables
-    #    db.create_all()
-
-    @app.before_first_request
-    def create_database():
-        db.create_all()
-
-    app.run(args.host, args.port, debug=True)
+    app.run(args.host, args.port)
 
 
 def create(app, args):
