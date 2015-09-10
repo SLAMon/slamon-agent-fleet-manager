2547c4f48498ca7c1947283c15b667293b7607ea
Jouni Kuusisto <jouni.kuusisto@f-secure.com>
2015-09-10 10:55:57 +0300

    Configuration using environment variables
    Implemented reading known configuration values from environment variables
    to simplify deploying AFM as a docker container: no additional files are required
    to be included in the image and each configuration parameter is configurable withing
    a docker-compose file.

--- slamon_afm/tests/afm_test.py
+++ slamon_afm/tests/afm_test.py
@@ -3,7 +3,7 @@ import logging
 
 from webtest import TestApp
 
-from slamon_afm.app import create_app
+from slamon_afm.app import create_app, _bool_from_str, _log_level_from_str
 from slamon_afm.models import db
 
 
@@ -27,3 +27,31 @@ class AFMTest(TestCase):
     def tearDown(self):
         db.drop_all()
         self.app_context.pop()
+
+
+class ConfigUtilTest(TestCase):
+    def testOkTrueValues(self):
+        for value in ('True', 'TRUE', 'true', 'Yes', 'yes', 'YES'):
+            self.assertTrue(_bool_from_str(value))
+
+    def testOkFalseValues(self):
+        for value in ('False', 'FALSE', 'false', 'No', 'no', 'NO'):
+            self.assertFalse(_bool_from_str(value))
+
+    def testNokValues(self):
+        for value in ('f', 't', '0', '1', 'nope', 'yep'):
+            with self.assertRaises(ValueError):
+                _bool_from_str(value)
+
+    def testOkLogLevels(self):
+        # known values (standard logging levels)
+        for value in ('CRITICAL', 'Error', 'warning', 'INFO', 'DEBUG', 'NOTSET'):
+            self.assertEqual(getattr(logging, value.upper()), _log_level_from_str(value))
+        # explicit integer values
+        for value in (-1, 0, 1, 50,100):
+            self.assertEqual(value, _log_level_from_str('{}'.format(value)))
+
+    def testNokLogLevels(self):
+        for value in ('f', 't', 'nope', 'yep'):
+            with self.assertRaises(ValueError):
+                _log_level_from_str(value)
